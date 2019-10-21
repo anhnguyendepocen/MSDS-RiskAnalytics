02e14684bd9f47ded23b4ef79da52894b0a29e4e
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-10-20 20:25:25 -0400

    Module 5 work
    Module 5 work

--- Module_05/time_series_lab.R
+++ Module_05/time_series_lab.R
@@ -8,3 +8,23 @@ par(mfrow = c(1,2))
 acf(y)
 acf(diff(y))
 
+Box.test(diff(y), lag = 10, type = "Ljung-Box")
+
+data(bmw, package = "evir")
+
+Box.test(bmw, lag = 5, type = "Ljung-Box")
+
+fitAR1 <- arima(bmw, order = c(1, 0, 0))
+summary(fitAR1)
+
+acf(bmw)
+
+Box.test(residuals(fitAR1), lag = 5, type = "Ljung-Box", fitdf = 1)
+
+fit <- arima(y, order = c(1, 0, 0))
+Box.test(fit$residuals, type = "Ljung", lag = 24, fitdf = 1)
+
+library(forecast)
+
+auto.arima(diff(y), max.p = 20, max.q = 0, d = 0, ic = "aic")
+
